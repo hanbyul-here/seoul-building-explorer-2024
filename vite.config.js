e3c1c942fee74fcd3afc1492a22acdaee20e158d
Hanbyul Jo <hanbyul@nyu.edu>
2024-02-12 08:36:14 -0500

    Add workaround to build for Compare control

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // For Compare Control
+  // https://github.com/vitejs/vite/issues/9703#issuecomment-1216662109
+  build: {
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    disabled: false,
+  }
 })
